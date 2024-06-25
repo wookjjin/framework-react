346034d34996a34ef58db83bf39430493dcbfdc7
김진욱 <jwook0318@iabacus.co.kr>
2024-06-25 15:10:07 +0900

    use typescript and eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import eslint from "@eslint/js";
+import tsEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import hooksPlugin from 'eslint-plugin-react-hooks';
+
+export default [
+  {
+    plugins: {
+      'react-hooks': hooksPlugin,
+      'react': react,
+      '@typescript-eslint': tsEslint,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'ESNext',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      }
+    },
+    rules: {
+      // 필요에 따라 여기에 규칙을 추가하세요.
+    }
+  },
+  // JS
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      ...eslint.configs.recommended.rules,
+      'no-undef': 'error',
+    }
+  },
+  // TS
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      ...tsEslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': 'error',
+    }
+  },
+  // React
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {
+      'react/prop-types': 'off',
+    }
+  },
+  {
+    ignores: ['dist', 'eslint.config.js']
+  },
+];
