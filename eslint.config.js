fb4cf988d0b4021e6b1b01a753149e91e1c6cfbe
김진욱 <jwook0318@gmail.com>
2024-07-03 11:45:28 +0900

    Update setting

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import functional from 'eslint-plugin-functional'
 import esImport from 'eslint-plugin-import'
 import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default [
   {
@@ -32,13 +34,16 @@ export default [
       // 필요에 따라 여기에 규칙을 추가하세요.
       ...tsEslint.configs.recommended.rules,
       ...eslint.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
       '@typescript-eslint/no-unused-vars': 'error',
       'no-undef': 'error',
       'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
       'max-len': ['error', { code: 120, tabWidth: 2, ignoreUrls: true }],
       quotes: ['error', 'single'],
-      indent: ['error', 2],
+      indent: ['error', 2, { 'SwitchCase': 1}],
       semi: ['error', 'never'],
+      'import/prefer-default-export': 'off',
+      'import/no-unresolved': 'off',
       'import/order': [
         'error',
         {
@@ -98,7 +103,24 @@ export default [
     rules: {
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
+      'react/display-name': 'off',
+      'react/function-component-definition': ['warn', { 'namedComponents': 'arrow-function' }],
+      'react/jsx-one-expression-per-line': 'off',
+      'react/jsx-filename-extension': ['error', { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+      'react/jsx-props-no-spreading': 'off',
+      'react/require-default-props': 'off',
+      'react/jsx-wrap-multilines': 'off',
+      'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/label-has-associated-control': [
+        'error',
+        {
+          'labelComponents': ['label'],
+          'labelAttributes': ['htmlFor'],
+          'controlComponents': ['input']
+        }
+      ]
     },
     settings: {
       react: {
@@ -116,5 +138,17 @@ export default [
   },
   {
     ignores: ['dist', 'eslint.config.js'],
+  },
+  /** jsx A11y */
+  {
+    files: ['**/*.{ts,tsx}'],
+    ...jsxA11y.flatConfigs.recommended,
+    languageOptions: {
+      ...jsxA11y.flatConfigs.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
   },
 ]
--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -32,15 +32,16 @@ const StyledLink = styled(RouterLink)`
   }
 `
 
-const Sidebar = styled.aside<{$isOpen: boolean}>`
+const Sidebar = styled.aside<{ $isOpen: boolean }>`
   background-color: #f4f4f4;
-  padding: ${(props) => (props.$isOpen ? '1rem' : '0')};
-  width: ${(props) => (props.$isOpen ? '250px' : '0')};
+  padding: ${props => (props.$isOpen ? '1rem' : '0')};
+  width: ${props => (props.$isOpen ? '250px' : '0')};
   height: 100vh;
   position: fixed;
   top: 0;
   left: 0;
-  box-shadow: ${(props) => (props.$isOpen ? '2px 0 5px rgba(0, 0, 0, 0.1)' : 'none')};
+  box-shadow: ${props =>
+    props.$isOpen ? '2px 0 5px rgba(0, 0, 0, 0.1)' : 'none'};
   margin-top: 4rem;
   z-index: 0;
   transition: width 0.3s ease;
@@ -48,7 +49,7 @@ const Sidebar = styled.aside<{$isOpen: boolean}>`
 `
 
 const Content = styled.main<{ $isOpen: boolean }>`
-  margin-left: ${(props) => (props.$isOpen ? '250px' : '0')};
+  margin-left: ${props => (props.$isOpen ? '250px' : '0')};
   margin-top: 4rem;
   padding: 1rem;
   height: calc(100vh - 4rem);
@@ -71,7 +72,7 @@ const DefaultLayout = () => {
     <Wrapper>
       <Header>
         <Button onClick={() => setIsOpen(!isSidebarOpen)}>
-          <Dehaze fontSize='large' />
+          <Dehaze fontSize="large" />
         </Button>
         <HeaderNav>
           <StyledLink to="/">Home</StyledLink>
